c82ba57f0d9df93472016213d4ed530b3ee4ffea
sameer4712 <sameerptmcgmail.com>
2025-07-25 14:24:31 +0530

    category delete

--- Controller/categoryController.js
+++ Controller/categoryController.js
@@ -1,4 +1,5 @@
 import CategoryDetails from "../models/categoryModel.js";
+import productDetails from "../models/productModel.js";
 
 export const category = async (req, res) => {
     const { name, description } = req.body
@@ -9,3 +10,20 @@ export const category = async (req, res) => {
     await addCategory.save()
     res.json("Category added successfully")
 }
+
+export const deleteCategory = async(req,res)=>{
+    try{
+        const categoryId = req.params.id
+        await productDetails.deleteMany({category:categoryId})
+        await CategoryDetails.deleteOne({_id:categoryId})
+        return res.json({message:"Category and all the products in the category"})
+    }
+    catch(err)
+    {
+        res.json(err)
+    }
+}
+
+// export const editCategory = async(req,res)=>{
+
+// }
