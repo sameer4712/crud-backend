c4ef04b2ab2f5e6489130bf3fc1ac9228cbabca6
sameer4712 <sameerptmc@gmail.com>
2025-09-15 15:37:34 +0530

    1st

--- app.js
+++ app.js
@@ -16,8 +16,12 @@ app.use(express.static("productImage"))
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cors({
-    origin:" http://localhost:5173",
-    credentials:true
+    origin: [
+        " http://localhost:5173",
+        " http://3.110.193.205:5173"
+
+    ],
+    credentials: true
 }))
 
 app.use(session(
@@ -38,8 +42,8 @@ app.use((req, res, next) => {
 })
 
 
-app.use("/user",userRoutes)
-app.use("/admin",adminRoutes)
+app.use("/user", userRoutes)
+app.use("/admin", adminRoutes)
 
 mongoose.connect(process.env.dburi).then(() => {
     app.listen((process.env.port), () => {
